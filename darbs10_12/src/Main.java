f391a110b396f2eedc5b457a9e741a10d65be3e2
fe1nmozik <okibro67@gmail.com>
2024-12-10 09:29:43 +0200

    Add files via upload

---
+++ darbs10_12/src/Main.java
@@ -0,0 +1,34 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        sum(12,58);
+
+
+
+    }
+    public static void sum(int a, int b){
+        int rez = a + b;
+        System.out.println("Skaitļu summa "+ a + " " + b + " ir " + rez );
+    }
+    public static void batter() {
+        System.out.println("Mix the dry ingredients.");
+        System.out.println("Cream the butter and sugar.");
+        System.out.println("Beat in the eggs.");
+        System.out.println("Stir in the dry ingredients.");
+    }
+    public static void baking (){
+        System.out.println("Set the oven temperature.");
+        System.out.println("Set the timer.");
+        System.out.println("Place a batch of cookies into the oven.");
+        System.out.println("Allow the cookies to bake.");
+        System.out.println("Set the oven temperature.");
+        System.out.println("Set the timer.");
+        System.out.println("Place a batch of cookies into the oven.");
+        System.out.println("Allow the cookies to bake.");
+    }
+    public static void decorate(){
+        System.out.println("Mix ingredients for frosting.");
+        System.out.println("Spread frosting and sprinkles.");
+    }
+}
